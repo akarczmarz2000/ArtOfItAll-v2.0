196785489bf8eac0175f00cd4fb2e7e01ed99bd7
Armand Józef Oliver Karczmarz <akarczmarz2000@gmail.com>
2020-07-29 14:19:26 +0000

    Added Toasts and wired them up

--- bag/views.py
+++ bag/views.py
@@ -12,7 +12,7 @@ def show_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity and size of the specified product to the shopping bag """
 
-    product = get_object_or_404(Product, pk=item_id)
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
