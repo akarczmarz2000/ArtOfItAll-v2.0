597fccb36548d7e565e5ce1d439c92be8216705f
Armand Józef Oliver Karczmarz <akarczmarz2000@gmail.com>
2021-02-16 15:34:57 +0000

    Deploying to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn art_of_it_all.wsgi:applicationq
--- art_of_it_all/settings.py
+++ art_of_it_all/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'vgycn-s(#rr+*m@m+9ur05=95+6omtx(bwja8x65jk_qfhl$l%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['artt-of-it-all-v2.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -113,14 +114,17 @@ WSGI_APPLICATION = 'art_of_it_all.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
