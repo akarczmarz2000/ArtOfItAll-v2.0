b1e86df1a86d745eba873faa246b8e7fb07e5803
Armand Józef Oliver Karczmarz <akarczmarz2000@gmail.com>
2021-04-14 09:43:02 +0000

    Added the necesserry code to make WhiteNoise to work to settings.py

--- art_of_it_all/settings.py
+++ art_of_it_all/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'art_of_it_all.urls'
@@ -164,6 +165,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
